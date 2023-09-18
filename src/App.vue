9f98cc01209fc3a8e10810402003f35d58621b60
Vinícius Blandy <viny19381@hotmail.com>
2023-09-17 23:01:03 -0300

    Componentes

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,63 @@
 <script setup>
-import { reactive } from 'vue';
+  import { reactive } from 'vue';
+  import Cabecalho from './components/Cabecalho.vue'
+  import Formulario from './components/Formulario.vue'
+  import ListaDeTarefas from './components/ListaDeTarefas.vue'
 
-const estado = reactive({
-  filtro: 'todas',
-  tarefaTemp: '',
-  tarefas: [
-    {
-      titulo: 'Estudar ES6',
-      finalizada: false
-    },
-    {
-      titulo: 'Ir a academia',
-      finalizada: true
-    },
-    {
-      titulo: 'Jantar',
-      finalizada: false
+  const estado = reactive({
+    filtro: 'todas',
+    tarefaTemp: '',
+    tarefas: [
+      {
+        titulo: 'Estudar ES6',
+        finalizada: false,
+      },
+      {
+        titulo: 'Estudar SASS',
+        finalizada: false,
+      },
+      {
+        titulo: 'Ir para a academia',
+        finalizada: true,
+      }
+    ]
+  })
+
+  const getTarefasPendentes = () => {
+    return estado.tarefas.filter(tarefa => !tarefa.finalizada)
+  }
+
+  const getTarefasFinalizadas = () => {
+    return estado.tarefas.filter(tarefa => tarefa.finalizada)
+  }
+
+  const getTarefasFiltradas = () => {
+    const { filtro } = estado;
+
+    switch (filtro) {
+      case 'pendentes':
+        return getTarefasPendentes();
+      case 'finalizadas':
+        return getTarefasFinalizadas();
+      default:
+        return estado.tarefas;
     }
-  ]
-})
-
-const getTarefaPendente = () => {
-  return estado.tarefas.filter(tarefa => !tarefa.finalizada)
-}
-
-const getTarefaFinalizadas = () => {
-  return estado.tarefas.filter(tarefa => tarefa.finalizada)
-}
-
-const getTarefasFiltradas = () => {
-  const {filtro} = estado
-
-  switch(filtro) {
-    case 'pendentes':
-    return getTarefaPendente()
-    case 'finalizadas': 
-    return getTarefaFinalizadas()
-    default: 
-    return estado.tarefas
   }
-}
 
-const adicionaTarefa = () => {
-  const tarefaNova = {
-    titulo: estado.tarefaTemp,
-    finalizada: false,
+  const cadastraTarefa = () => {
+    const tarefaNova = {
+      titulo: estado.tarefaTemp,
+      finalizada: false,
+    }
+    estado.tarefas.push(tarefaNova);
+    estado.tarefaTemp = '';
   }
-  estado.tarefas.push(tarefaNova)
-  estado.tarefaTemp = ''
-}
 </script>
 
 <template>
-<div class="container">
-  <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-  <h1>Minhas tarefas</h1>
-<p>
-  Você possui {{ getTarefaPendente().length }} tarefas pendentes
-</p>
-</header>
-<form @submit.prevent="adicionaTarefa">
-  <div class="row">
-  <div class="col">
-    <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required class="form-control" type="text" placeholder="Digite aqui a descrição da tarefa">
+  <div class="container">
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length" />
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa" />
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()" />
   </div>
-  <div class="col-md-2">
-    <button type="submit" class="btn btn-primary">Cadastrar</button>
-  </div>
-  <div class="col-md-2">
-    <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-      <option value="todas">Todas tarefas</option>
-      <option value="pendentes">Pendentes</option>
-      <option value="finalizadas">Finalizadas</option>
-    </select>
-  </div>
-</div>
-</form>
-<ul class="list-group mt-4">
-  <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-    <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
-    <label :class="{finalizada: tarefa.finalizada}" class="ms-3" :for="tarefa.titulo">
-      {{ tarefa.titulo }}
-    </label>
-  </li>
-</ul>
-</div>
 </template>
-
-<style scoped>
-.finalizada {
-  text-decoration: line-through;
-}
-</style>
